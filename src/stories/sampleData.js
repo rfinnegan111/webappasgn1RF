d45f7cef73355c59af930e62f821e7a2dceaea81
Rebecca Finnegan <20093862@mail.wit.ie>
2022-10-07 14:49:32 +0100

    Production Countires Amended

--- src/stories/sampleData.js
+++ src/stories/sampleData.js
@@ -58,8 +58,12 @@ export default {
     ],
     production_countries: [
       {
-        iso_3166_1: "US",
-        name: "United States of America",
+        iso_3166_1: "FR",
+        name: "France",
+      },
+      {
+        iso_3166_2: "LTH",
+        name: "Lithuania",
       },
     ],
     release_date: "2017-12-13",
