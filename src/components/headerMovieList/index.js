437846ba95848e198ca3865b0846b3247e599a36
Rebecca Finnegan <20093862@mail.wit.ie>
2022-11-02 19:53:53 +0000

    Lab work catch up.

--- src/components/headerMovieList/index.js
+++ src/components/headerMovieList/index.js
@@ -1,38 +1,20 @@
 import React from "react";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import Paper from "@mui/material/Paper";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
+import {createRoot} from "react-dom/client";
+import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
+import HomePage from "./pages/homePage";
+import MoviePage from "./pages/movieDetailsPage";
 
-const Header = (props ) => {
-  const title = props.title
+const App = () => {
   return (
-    <Paper 
-      component="div" 
-      sx={{
-        display: "flex",
-        justifyContent: "space-around",
-        flexWrap: "wrap",
-        marginBottom: 1.5,
-      }}
-      >
-      <IconButton
-        aria-label="go back"
-      >
-        <ArrowBackIcon color="primary" fontSize="large" />
-      </IconButton>
-
-      <Typography variant="h4" component="h3">
-        {title}
-      </Typography>
-      <IconButton
-        aria-label="go forward"
-      >
-        <ArrowForwardIcon color="primary" fontSize="large" />
-      </IconButton>
-    </Paper>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/movies/:id" element={<MoviePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={ <Navigate to="/" /> } />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
-export default Header;
+const rootElement = createRoot( document.getElementById("root") )
+rootElement.render(<App /> );
