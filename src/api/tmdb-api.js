f809360757251496525a3fe5bac29916d1fa0e51
Rebecca Finnegan <20093862@mail.wit.ie>
2022-12-02 19:19:00 +0000

    Adding the APIs

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -1,3 +1,5 @@
+//MOVIES
+
 export const getMovies = () => {
     return fetch(
       `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
@@ -81,4 +83,104 @@ export const getMovies = () => {
       )
         .then(res => res.json())
         .then(json => json.results);
+    };
+
+    //ACTORS
+
+    export const getActors = () => {
+      return fetch(
+        `https://api.themoviedb.org/3/person/popular?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+      ).then((response) => {
+        if (!response.ok) {
+          throw new Error(response.json().message);
+        }
+        return response.json();
+      })
+      .catch((error) => {
+         throw error
+      });
+    };
+
+    export const getActor = (args) => {
+      // console.log(args)
+      const [, idPart] = args.queryKey;
+      const { id } = idPart;
+      return fetch(
+        `https://api.themoviedb.org/3/person/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
+      ).then((response) => {
+        if (!response.ok) {
+          throw new Error(response.json().message);
+        }
+        return response.json();
+      })
+      .catch((error) => {
+        throw error
+     });
+    };
+
+    export const getActorImages = ({ queryKey }) => {
+      const [, idPart] = queryKey;
+      const { id } = idPart;
+      return fetch(
+        `https://api.themoviedb.org/3/person/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
+      ).then( (response) => {
+        if (!response.ok) {
+          throw new Error(response.json().message);
+        }
+        return response.json();
+    
+      })
+      .catch((error) => {
+        throw error
+     });
+    };
+
+    //TV SERIES
+
+    export const getTvs = () => {
+      return fetch(
+        `https://api.themoviedb.org/3/tv/top_rated?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+      ).then((response) => {
+        if (!response.ok) {
+          throw new Error(response.json().message);
+        }
+        return response.json();
+      })
+      .catch((error) => {
+         throw error
+      });
+    };
+
+    export const getTv = (args) => {
+      // console.log(args)
+      const [, idPart] = args.queryKey;
+      const { id } = idPart;
+      return fetch(
+        `https://api.themoviedb.org/3/tv/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
+      ).then((response) => {
+        if (!response.ok) {
+          throw new Error(response.json().message);
+        }
+        return response.json();
+      })
+      .catch((error) => {
+        throw error
+     });
+    };
+
+    export const getTvImages = ({ queryKey }) => {
+      const [, idPart] = queryKey;
+      const { id } = idPart;
+      return fetch(
+        `https://api.themoviedb.org/3/tv/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
+      ).then( (response) => {
+        if (!response.ok) {
+          throw new Error(response.json().message);
+        }
+        return response.json();
+    
+      })
+      .catch((error) => {
+        throw error
+     });
     };
